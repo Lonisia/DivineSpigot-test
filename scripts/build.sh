33158e8a8cc7dd031869854618712913aecf78bc
Tommy Teschner <tommynormanteschner@gmail.com>
2024-08-20 11:48:08 -0700

    Initial commit

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+(
+set -e
+basedir="$(cd "$1" && pwd -P)"
+gitcmd="git -c commit.gpgsign=false"
+
+$gitcmd submodule update --init --recursive
+cd "$basedir"
+
+if [ "$2" == "--setup" ] || [ "$2" == "--jar" ]; then
+    ./scripts/remap.sh "$basedir"
+    ./scripts/decompile.sh "$basedir"
+    ./scripts/init.sh "$basedir"
+fi
+./scripts/applyPatches.sh "$basedir" "$2"
+
+if [ "$2" == "--jar" ]; then
+    ./gradlew build && ./scripts/paperclip.sh "$basedir"
+fi
+) || exit 1
